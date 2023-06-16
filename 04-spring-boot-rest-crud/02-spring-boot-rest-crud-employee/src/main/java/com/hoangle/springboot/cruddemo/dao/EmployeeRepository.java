7b480374bad8281fa83c3e7c3b1babe81daaedc7
Lê Minh Hoàng <leminhhoangk3@gmail.com>
2023-06-16 16:39:49 +0700

    add config pagination sort for Spring Data REST

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.hoangle.springboot.cruddemo.dao;
 import com.hoangle.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
