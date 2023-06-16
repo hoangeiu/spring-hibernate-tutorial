a5776191eeed3846a24dbfc9bc107ed3516f9ca2
Lê Minh Hoàng <leminhhoangk3@gmail.com>
2023-06-16 16:26:49 +0700

    using Spring Data REST for auto create simple apis

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeDAO.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeDAO.java
@@ -5,11 +5,11 @@ import com.hoangle.springboot.cruddemo.entity.Employee;
 import java.util.List;
 
 public interface EmployeeDAO {
-    List<Employee> findAll();
-
-    Employee findById(int theId);
-
-    Employee save(Employee theEmployee);
-
-    void deleteById(int theId);
+//    List<Employee> findAll();
+//
+//    Employee findById(int theId);
+//
+//    Employee save(Employee theEmployee);
+//
+//    void deleteById(int theId);
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hoangle/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -8,41 +8,41 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+//@Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO {
-    private EntityManager entityManager;
-
-    @Autowired
-    public EmployeeDAOJpaImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
-
-        List<Employee> employees = theQuery.getResultList();
-
-        return employees;
-    }
-
-    @Override
-    public Employee findById(int theId) {
-        Employee theEmployee = entityManager.find(Employee.class, theId);
-        return theEmployee;
-    }
-
-    @Override
-    public Employee save(Employee theEmployee) {
-        Employee dbEmployee = entityManager.merge(theEmployee);
-
-        return dbEmployee;
-    }
-
-    @Override
-    public void deleteById(int theId) {
-        Employee theEmployee = entityManager.find(Employee.class, theId);
-
-        entityManager.remove(theEmployee);
-    }
+//    private EntityManager entityManager;
+//
+//    @Autowired
+//    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
+//
+//    @Override
+//    public List<Employee> findAll() {
+//        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+//
+//        List<Employee> employees = theQuery.getResultList();
+//
+//        return employees;
+//    }
+//
+//    @Override
+//    public Employee findById(int theId) {
+//        Employee theEmployee = entityManager.find(Employee.class, theId);
+//        return theEmployee;
+//    }
+//
+//    @Override
+//    public Employee save(Employee theEmployee) {
+//        Employee dbEmployee = entityManager.merge(theEmployee);
+//
+//        return dbEmployee;
+//    }
+//
+//    @Override
+//    public void deleteById(int theId) {
+//        Employee theEmployee = entityManager.find(Employee.class, theId);
+//
+//        entityManager.remove(theEmployee);
+//    }
 }
